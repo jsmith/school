7d12ad8666a0adc8b52a52774644917285c1ca54
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-05-30 20:54:05 -0300

    init midterm practice

--- cs2263/midterm/1.c
+++ cs2263/midterm/1.c
@@ -1,7 +1,34 @@
-//
-// Created by jacob on 29/05/18.
-//
+#include <stdio.h>
+
+int function() {
+  int a = 5;
+  return a;
+}
 
 int main() {
+  printf("helloworld\n");
 
+  // formatting float
+  float f = 0.1;
+  printf("%f\n", f);
+
+  int d;
+  char c;
+  while(1) {
+    scanf("%d%c", &d, &c);
+    printf("Scanned: %d\n", d);
+    printf("This is a test @ scanf! %d\n", d);
+    if(c == '\n') {
+      break;
+    }
+  }
+
+  // test fgets
+  int test[5];
+  test[0] = 56;
+  int *temp = test;
+
+  printf("Printing pointer value: %d\n", *temp);
+  printf("Printing pointer value with index: %d\n", temp[55]);
+  printf("Calling function: %d\n", function());
 }
