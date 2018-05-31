4a4fc52448435fb035c0da4a5f08229b81e8da9e
Jacob Smith <jacob.smith@eigeninnovations.com>
2018-05-31 10:08:18 -0300

    finished midterm questions

--- cs2263/midterm/1.c
+++ cs2263/midterm/1.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int function() {
   int a = 5;
   return a;
+}
+
+int length(char *s){
+  char *p = s; //warning if p not const
+  for(; *p ;p++);
+  return p - s;
+}
+
+void print(int* arr, int n) {
+  for(int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
 }
 
 int main() {
@@ -12,16 +27,16 @@ int main() {
   float f = 0.1;
   printf("%f\n", f);
 
-  int d;
-  char c;
-  while(1) {
-    scanf("%d%c", &d, &c);
-    printf("Scanned: %d\n", d);
-    printf("This is a test @ scanf! %d\n", d);
-    if(c == '\n') {
-      break;
-    }
-  }
+  // int d;
+  // char c;
+  // while(1) {
+  //   scanf("%d%c", &d, &c);
+  //   printf("Scanned: %d\n", d);
+  //   printf("This is a test @ scanf! %d\n", d);
+  //   if(c == '\n') {
+  //     break;
+  //   }
+  // }
 
   // test fgets
   int test[5];
@@ -31,4 +46,21 @@ int main() {
   printf("Printing pointer value: %d\n", *temp);
   printf("Printing pointer value with index: %d\n", temp[55]);
   printf("Calling function: %d\n", function());
+
+  char *a ="One", *b="Two";
+  printf("%s vs %s: %s\n", a, b, strcmp(a, b) ? "False" : "True");
+
+  a = "0123456789 \t\na";
+  printf("The length of %s is %d\n", a, length(a));
+  int index = strstr(a, "8") - a;
+  printf("8 can be found in the %dth index\n", index);
+
+  int *malloced = malloc(sizeof(int)*5);
+  malloced[0] = 0;
+  malloced[1] = 1;
+  malloced[2] = 2;
+  malloced[3] = 3;
+  malloced[4] = 666;
+  print(malloced, 10);
+  free(malloced);
 }
