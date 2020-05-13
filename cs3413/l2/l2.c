1316bb3b3c544ccbbb5c64266dfc9c89cc30520d
Jacob Smith <jacob.smith@unb.ca>
2020-05-13 19:38:19 -0300

    l2

--- cs3413/l2/l2.c
+++ cs3413/l2/l2.c
@@ -1,72 +1,117 @@
+#include <pthread.h>
 #include <stdio.h>
-#include <sys/wait.h>
 #include <unistd.h>
-#include <stdbool.h>
+#include <limits.h>
+#define WRITE 1
+#define READ 0
+
+int fd1[2];
+int fd2[2];
+int fd3[2];
+
+int minValue = INT_MAX;
+int maxValue = INT_MIN;
+int averageValue = 0;
+
+void* min() {
+  int k;
+  while (1) {
+    read(fd1[READ], &k, sizeof(k));
+    if (k == 0) {
+      break;
+    }
+
+    if (k < minValue) {
+      minValue = k;
+    }
+  }
+  return 0;
+}
+
+void* max() {
+  int k;
+  while (1) {
+    read(fd2[READ], &k, sizeof(k));
+    if (k == 0) {
+      break;
+    }
+
+    if (k > maxValue) {
+      maxValue = k;
+    }
+  }
+  return 0;
+}
+
+void* average() {
+  int k;
+  int sum = 0;
+  int n = 0;
+  while (1) {
+    read(fd3[READ], &k, sizeof(k));
+    if (k == 0) {
+      break;
+    }
+
+    sum += k;
+    n++;
+  }
+
+  averageValue = sum / n;
+  return 0;
+}
 
 // Note this ignores -1 pids
 int main() {
-  int pid1 = 0; // child pid
-  int p1[2]; // child 1 pipe file descriptors
+  pipe(fd1);
+  pipe(fd2);
+  pipe(fd3);
 
-  int pid2 = 0;
-  int p2[2];
+  pthread_t t1;
+  pthread_t t2;
+  pthread_t t3;
 
-  int pid3 = 0;
-  int p3[2];
-
-  pipe(p1);
-  pipe(p2);
-  pipe(p3);
-
-  printf("Creating p1!\n");
-  pid1 = fork();
-  bool is1 = pid1 == 0;
-  bool isParent = pid1 > 0;
-
-  if (isParent) {
-    printf("Creating p2!\n");
-    pid2 = fork();
-  }
-
-  bool is2 = isParent && pid2 == 0;
-  isParent = isParent && pid2 > 0;
-  if (isParent) {
-    printf("Creating p3!\n");
-    pid3 = fork();
-  }
-
-  bool is3 = isParent && pid3 == 0;
-  isParent = isParent && pid3 > 0;
+  printf("Creating 3 child threads!\n");
+  pthread_create(&t1, NULL, &min, NULL);
+  pthread_create(&t2, NULL, &max, NULL);
+  pthread_create(&t3, NULL, &average, NULL);
 
   int k;
-  if (is1) {
-    printf("I'm the first!\n");
-    while (true) {
-      read(pipe_r, &k, sizeof(k));
-      printf("GOT %d\n", k);
-      if (k == 0) {
-        break;
-      }
-      
-    }
-  } else if (is2) {
-    printf("I'm the second!\n");
-  } else if (is3) {
-    printf("I'm the third!\n");
-  } else {
-    printf("I'm the parent!\n");
-    while (scanf("%d", &k) == 1) {
-      printf("read number: %d\n", k);
-      write(pipe_w, &k, sizeof(k));
-    }
+  while (scanf("%d", &k) == 1) {
+    printf("read number: %d\n", k);
+    write(fd1[WRITE], &k, sizeof(k));
+    write(fd2[WRITE], &k, sizeof(k));
+    write(fd3[WRITE], &k, sizeof(k));
 
-    printf("Waiting for 3 child processes!\n");
-    wait(&pid1);
-    wait(&pid2);
-    wait(&pid3);
+    if (k == 0) {
+      break;
+    }
   }
 
-  close(p1);
-  close(p2);
-  close(p3);
+  // If use piped in values and never specified 0 at the end
+  // This wasn't specified in the assignment but I handle this situation just in case
+  if (k != 0) {
+    k = 0;
+    write(fd1[WRITE], &k, sizeof(k));
+    write(fd2[WRITE], &k, sizeof(k));
+    write(fd3[WRITE], &k, sizeof(k));
+  }
+
+
+  printf("Waiting for 3 child threads!\n");
+  pthread_join(t1, NULL);
+  pthread_join(t2, NULL);
+  pthread_join(t3, NULL);
+
+  printf("\nResults\n");
+  printf("The average value is %d\n", averageValue);
+  printf("The minimum value is %d\n", minValue);
+  printf("The maximum value is %d\n", maxValue);
+
+  close(fd1[READ]);
+  close(fd1[WRITE]);
+  close(fd2[READ]);
+  close(fd2[WRITE]);
+  close(fd3[READ]);
+  close(fd3[WRITE]);
 }
